2be839e1e1f9c0fd83b224c797f18efbfae480bb
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2021-10-23 17:24:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
 array1 = ['pen', 'paper', 'book', 'bottle'];
 random_no = Math.floor((Math.random()*array1.length)+1);
 element_of_array = array1(random_no);
-document.getElementById("sketch").innerHTML = "Sketch to be Drawn: " + element_of_array;
+document.getElementById("sketch").innerHTML = element_of_array;
 timer_counter = 0;
 timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+function updateCanvas(){
+    background("white");
+}
+
+function setup(){
+    canvas = createCanvas(280, 280, 0, 0);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw(){
+    check_sketch();
+    if(drawn_sketch = document.getElementById("sketch")){
+        answer_holder = "set";
+        score++;
+        document.getElementById("score").innerHTML = "Score: " + score;
+    }
+}
+
+function check_sketch(){
+    timer++;
+    document.getElementById("timer").innerHTML = "Timer: " + timer;
+    console.log(timer)
+    if(timer = 400){
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if((answer_holder = "set") || (timer = 400)){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
